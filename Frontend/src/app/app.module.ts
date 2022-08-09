ec6c8d441be0a9e22ffc3f61da3efc0270330651
Kishan v <vaishnanik@gmail.com>
2022-08-09 22:51:18 +0530

    user signUp layout

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { commonModule } from './common-module/common-module.module';
+import { commonModule } from './common/common-module.module';
 
 @NgModule({
   declarations: [
--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LoginComponent } from './login/login.component';
-import { commonModule } from '../common-module/common-module.module';
+import { commonModule } from '../common/common-module.module';
 import { UserSignupComponent } from './user-signup/user-signup.component';
 import { InstituteSignupComponent } from './institute-signup/institute-signup.component';
 
