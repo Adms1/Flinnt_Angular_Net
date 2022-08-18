80547c62a1f3391768f5b8240df0a1b7e9d763a9
Kishan v <vaishnanik@gmail.com>
2022-08-18 15:37:32 +0530

    instituteSignUp form validation and added service

--- Frontend/src/app/home/institute-signup/institute-signup.component.ts
+++ Frontend/src/app/home/institute-signup/institute-signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-institute-signup',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./institute-signup.component.css']
 })
 export class InstituteSignupComponent implements OnInit {
-
-  constructor() { }
+  instituteSignUpForm = {} as FormGroup;
+  formSubmitted = false;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
+    this.createSignUpForm();
   }
 
+  createSignUpForm(){
+    this.instituteSignUpForm = this.formBuilder.group({
+			firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailId: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      instituteName: ['', Validators.required],
+      pageName: ['', Validators.required],
+		});
+  }
+
+  onSignUpSubmit() {
+    this.formSubmitted = true;
+    if(this.instituteSignUpForm.invalid) return;
+
+		let data = JSON.stringify(this.instituteSignUpForm.value);
+		console.log('-----SignUp JSON Format-----');
+		console.log(data);
+    // APIs
+
+    this.resetTeamForm();
+	}
+	resetTeamForm() {
+		this.instituteSignUpForm.reset();
+	}
 }
