64ab3b831d28cecb50aec7cc6cd7ab4391b43cad
Kishan v <vaishnanik@gmail.com>
2022-09-23 08:25:53 +0530

    logout menu

--- Frontend/src/app/shared/header/header.component.ts
+++ Frontend/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,26 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() roleId:number = 0;
   @Input() userObj:any = {};
-
-  constructor() { }
+  menuClicked = false;
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  onLogout(){
+    localStorage.clear();
+    this.route.navigate(['']);
+  }
+
+  onAncherClick(event? : Event){
+    const menu = event.currentTarget["parentElement"].querySelector(".user_menu");
+    if(!this.menuClicked){
+      this.menuClicked = true;
+      menu.classList.add("display-menu");
+    }
+    else{
+      this.menuClicked = false;
+      menu.classList.remove("display-menu");
+    }
+  }
 }
